6e53efa67e564dbe97040cf54e8c2b80f0a9a804
Kb-tay <taykaiboon12@gmail.com>
2023-07-14 18:48:21 +0800

    navbar phone ui

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,49 @@
 import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react";
 import React, { useState } from "react";
+import { AiOutlineMenu } from 'react-icons/ai'
 
 export default function NavBar() {
   const session = useSession()
   const user = session.data?.user
   
   const [active, setActive] = useState(0)
+  const [drop, setDrop] = useState(false)
   
   const buttonStyle = { unfocused: "bg-gray-900",   focused: "bg-gray-500"}
 
   return(
     <nav className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
+
+          <div className="sm:hidden pl-2">
+            <AiOutlineMenu className="h-8 w-8 text-white" onClick={() => setDrop(!drop)}/>
+            { 
+              drop ? user == null 
+                ? <></>
+                :
+                <div className="absolute left-0 top-14 z-10 mt-2 w-40 divide-y divide-slate-400 rounded-md bg-slate-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-xl">
+                  <div className="flex flex-col pl-2 space-y-1">
+                    <Link href="/">
+                      <button className={`${active == 0 ? "font-bold text-slate-400" : "text-white"}`} onClick={() => {setDrop(false); setActive(0)}}>New Spending</button>
+                    </Link>
+                    <Link href="/profile">
+                      <button className={`${active == 1 ? "font-bold text-slate-400" : "text-white"}`} onClick={() => {setDrop(false); setActive(1)}}>Profile</button>
+                    </Link>
+                    <Link href="/search">
+                      <button className={`${active == 2 ? "font-bold text-slate-400" : "text-white"}`} onClick={() => {setDrop(false); setActive(2)}}>Search Friends</button>
+                    </Link>
+                  </div>
+                  <div className="pl-2">
+                    <Link href='/'>
+                      <button className='p1-2 text-white' onClick={() => void signOut({callbackUrl : "/"})}>Sign Out</button> 
+                    </Link> 
+                  </div>
+                </div> 
+                : <></>
+            }
+          </div>
 
           <div className="hidden sm:ml-6 sm:block">
             <div className="flex flex-row space-x-3 ">
